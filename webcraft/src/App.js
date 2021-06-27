8f72939a69ca17dc488b86765880f8bed2801fb5
anas1503 <md.anas1503@gmail.com>
2021-06-27 17:41:23 +0530

    added gamecards component

--- webcraft/src/App.js
+++ webcraft/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import GameCards from './components/gamecards/GameCards';
 
 function App() {
   return (
     <div className="App">
-    Hello World!
+    <GameCards />
     </div>
   );
 }
---
+++ webcraft/src/components/gamecards/GameCards.js
@@ -0,0 +1,81 @@
+import React from 'react'
+const GameCards = () => {
+    return (
+        <div className="container">
+
+            <h3>TRENDING NOW</h3>
+            <div className="row">
+            <div className="col-sm-3 py-3 py-sm-0">             
+               <div class="card" style={{ width: "18rem", height: "23rem" }}>
+                    <img src="/assets/images/Valorant.png" class="card-img-top" style={{ width: "100%", height: "23rem" }} alt="..." />
+                    <div class="card-body px-0">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+                </div>
+                </div>
+                <div className="col-sm-3 py-3 py-sm-0">             
+
+                <div class="card" style={{ width: "18rem", height: "23rem" }}>
+                    <img src="/assets/images/pubg.png" class="card-img-top" style={{ width: "100%", height: "23rem" }} alt="..." />
+                    <div class="card-body px-0">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+                </div>
+                </div>
+
+                <div className="col-sm-3 py-3 py-sm-0">             
+                <div class="card" style={{ width: "18rem", height: "23rem" }}>
+                    <img src="/assets/images/lastofus.png" class="card-img-top" style={{ width: "100%", height: "23rem" }} alt="..." />
+                    <div class="card-body px-0">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+                </div>
+                </div>
+
+                <div className="col-sm-3 py-3 py-sm-0">             
+                <div class="card" style={{ width: "18rem", height: "23rem" }}>
+                    <img src="/assets/images/GTA5.png" class="card-img-top" style={{ width: "100%", height: "23rem" }} alt="..." />
+                    <div class="card-body px-0">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+                </div>
+                </div>
+                {/* <div className="col-sm-3 py-3 py-sm-0">             
+                <div class="card" style={{ width: "18rem", height: "23rem" }}>
+                    <img src="/assets/images/daysgone.png" class="card-img-top" style={{ width: "100%", height: "23rem" }} alt="..." />
+                    <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+                </div>
+                </div>
+                <div className="col-sm-3 py-3 py-sm-0">             
+                <div class="card" style={{ width: "18rem", height: "23rem" }}>
+                    <img src="/assets/images/cyberpunk.png" class="card-img-top" style={{ width: "100%", height: "23rem" }} alt="..." />
+                    <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+                </div>
+                </div>
+                <div className="col-sm-3 py-3 py-sm-0">             
+                <div class="card" style={{ width: "18rem", height: "23rem" }}>
+                    <img src="/assets/images/battlefield.png" class="card-img-top" style={{ width: "100%", height: "23rem" }} alt="..." />
+                    <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+                </div>
+                </div>
+                <div className="col-sm-3 py-3 py-sm-0">             
+                <div class="card" style={{ width: "18rem", height: "23rem" }}>
+                    <img src="/assets/images/AmongUs.png" class="card-img-top" style={{ width: "100%", height: "23rem" }} alt="..." />
+                    <div class="card-body">
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+  </div>
+                </div>
+                </div> */}
+            </div>
+
+        </div>
+    )
+}
+
+export default GameCards
