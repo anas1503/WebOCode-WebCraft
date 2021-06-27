30179a0de15b1cee4c500b0b8d6a6e4b2b792118
Shivam16Gupta <shivamgupta1281@gmail.com>
2021-06-27 21:58:51 +0530

    added flex box to videos

--- webcraft/src/components/improve/Improve.jsx
+++ webcraft/src/components/improve/Improve.jsx
@@ -5,42 +5,49 @@ const Improve = () => {
     <>
       <div className="improve">
         <div className="improveContent">
-          <div classname="videoCards">
-            <div class="card">
+          <h1>IMPROVE YOUR GAME</h1>
+          <div className="videoCards">
+
+            <div className="video-card">
               <iframe
-                className="card-img-top"
+                className="card-media"
                 src={`https://www.youtube.com/embed/Pj4Ch2vZ4bc`}
                 frameBorder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowFullScreen
                 title="Embedded youtube"
               />
-              <div class="card-body">
-                <p class="card-text">
-                  Some quick example text to build on the card title and make up
-                  the bulk of the card's content.
-                </p>
-              </div>
+            <p>
+              Learn to aim from best players
+            </p>
+            </div>
+            <div className="video-card">
+              <iframe
+                className="card-media"
+                src={`https://www.youtube.com/embed/PMg2SyiuGFU`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Embedded youtube"
+              />
+              <p>
+                Learn how best players move
+              </p>
+            </div>
+            <div className="video-card">
+              <iframe
+                className="card-media"
+                src={`https://www.youtube.com/embed/2FXc8jZ5Or4`}
+                frameBorder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowFullScreen
+                title="Embedded youtube"
+              />
+              <p>
+                Master your agent
+              </p>
             </div>
 
-            <iframe
-              width="853"
-              height="480"
-              src={`https://www.youtube.com/embed/PMg2SyiuGFU`}
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-              title="Embedded youtube"
-            />
-            <iframe
-              width="853"
-              height="480"
-              src={`https://www.youtube.com/embed/2FXc8jZ5Or4`}
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-              title="Embedded youtube"
-            />
           </div>
         </div>
       </div>
