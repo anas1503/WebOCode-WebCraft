6f61e40bb2ea51a6ca4cb91d05bfdbedc1e61030
Shivam16Gupta <shivamgupta1281@gmail.com>
2021-06-27 14:44:16 +0530

    added navbar and landing

--- webcraft/src/App.js
+++ webcraft/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Landing from './components/header/Landing';
 
 function App() {
   return (
     <div className="App">
-    Hello World!
+    <Landing/>
     </div>
   );
 }
